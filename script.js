79eaeac459e3b8b8885e6b44c16daf31ce384b9b
dangkhoaln <lndangkhoa19@gmail.com>
2023-08-12 16:30:48 +0700

    Define getComputerChoice()

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const Choice = (choice) => {
     // Choice is one of 0,1,2 which indicates rock, paper, scissor respectively
     const getChoice = () => choice;
-    const winAgainst = () => choice-1 >= 0 ? choice-1 : 2;
-    const loseAgainst = () => choice+1 <= 2 ? choice+1 : 0;
-    return {getChoice,winAgainst,loseAgainst}
+    const winAgainst = () => choice - 1 >= 0 ? choice - 1 : 2;
+    const loseAgainst = () => choice + 1 <= 2 ? choice + 1 : 0;
+    return { getChoice, winAgainst, loseAgainst }
+}
+
+const getComputerChoice = () => {
+    const randomInt = Math.floor(Math.random()*3);
+    return Choice(randomInt);
 }
