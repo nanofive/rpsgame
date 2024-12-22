e31a77cdf34672b76d3e041403776e51e86354d3
nanosix <nanosixfive@gmail.com>
2024-12-22 16:37:33 +0700

    Complete 1st UI version

--- script.js
+++ script.js
@@ -18,28 +18,84 @@ const getComputerChoice = () => {
 }
 
 const playRound = (playerSelection, computerSelection) => {
+    let result, message;
     switch (playerSelection.getChoice()) {
         case computerSelection.getChoice():
-            return "It's a draw";
+            result = "DRAW";
+            message = "It's a draw";
+            break;
         case computerSelection.loseAgainst():
-            return "You win. " + choiceToString[playerSelection.getChoice()] + " beats " + choiceToString[computerSelection.getChoice()];
+            result = "WIN";
+            message = "You win. " + choiceToString[playerSelection.getChoice()] + " beats " + choiceToString[computerSelection.getChoice()];
+            break;
         case computerSelection.winAgainst():
-            return "You lose. " + choiceToString[computerSelection.getChoice()] + " beats " + choiceToString[playerSelection.getChoice()];
+            result = "LOSE";
+            message = "You lose. " + choiceToString[computerSelection.getChoice()] + " beats " + choiceToString[playerSelection.getChoice()];
+            break;
+    }
+    return {result, message};
+}
+
+const printResult = (playerScore, computerScore, round) => {
+    const resultElem = document.querySelector("#result");    
+    resultElem.textContent = round.message; 
+    const scoreElem = document.querySelector("#score");    
+    scoreElem.textContent = "Player vs Computer score: " + playerScore + " - " + computerScore;
+}
+
+const printFinalResult = (winner) => {
+    const finalResultElem = document.querySelector("#final-result");
+    if (winner == "YOU") {
+        finalResultElem.textContent = "Congratulations! You won!";
+    } else {
+        finalResultElem.textContent = "You lost! Good luck next time.";
     }
 }
 
 const game = () => {
-    for (let i = 0; i < 5; i++) {
-        let playerInput;
-        const availableInput = [0, 1, 2];
-        while (!availableInput.includes(playerInput)) {
-            playerInput = parseInt(prompt("Enter one of: 0 means Rock, 1 means Paper, 2 means Scissor"));
+    const maxScore = 5;
+    let playerScore = 0;
+    let computerScore = 0;
+    const choicesElem = document.querySelector("#choices");
+    choicesElem.addEventListener('click', (event) => {
+        if (playerScore < maxScore && computerScore < maxScore) {
+            let target = event.target;
+            let playerSelection;
+            switch(target.id) {
+                case 'rock':
+                    playerSelection = Choice(0);
+                    break;
+                case 'paper':
+                    playerSelection = Choice(1);
+                    break;
+                case 'scissor':
+                    playerSelection = Choice(2);
+                    break;
+                default:
+                    return;
+            }
+            const computerSelection = getComputerChoice();
+            let round = playRound(playerSelection, computerSelection);
+            switch(round.result) {
+                case "WIN":
+                    playerScore += 1;
+                    break;
+                case "LOSE":
+                    computerScore += 1;
+                    break;
+                default:
+                    break;
+            }
+            printResult(playerScore, computerScore, round);
+            if (playerScore == 5) {
+                printFinalResult("YOU");
+            } else {
+                printFinalResult("COMPUTER");
+            }
+        } else {
+            return;
         }
-        const playerSelection = Choice(playerInput);
-        const computerSelection = getComputerChoice();
-        alert(playRound(playerSelection, computerSelection));
-    }
-    alert("Finished!");
+    });
 }
 
 game();
